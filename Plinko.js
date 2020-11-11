3f853e7886a62abe449fb6952cb6f26c297eb467
ShanykaBhandary <69717003+ShanykaBhandary@users.noreply.github.com>
2020-11-11 18:45:03 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,18 @@
+class Plinko{
+    constructor(x,y,width,height){
+      var options={
+        isStatic:true
+      
+      }  
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height)
+    }
+} 
